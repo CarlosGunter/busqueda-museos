082186e10de02a9a3f6f530295ea447f619da6a6
CarlosGunter <carlosgt1702@gmail.com>
2025-03-21 00:04:58 -0600

    style: UI de las card mejoradas

--- src/functional-components/CardMuseum.tsx
+++ src/functional-components/CardMuseum.tsx
@@ -23,17 +23,19 @@ function CardMuseum({
 }: TestMuseo){
 
   return (
-    <article className="rounded-3xl bg-bg-light shadow-md lg:grid lg:grid-cols-5 lg:gap-3">
-      <div className="relative h-45 overflow-hidden lg:col-start-4 lg:col-span-2 lg:row-[1]">
+    <article
+    className="rounded-3xl bg-bg-light shadow-md lg:grid lg:grid-cols-5 lg:gap-3"
+    style={{
+      contentVisibility: 'auto',
+    }}
+    >
+      <div className="relative h-45 overflow-hidden lg:col-start-4 lg:col-span-2 lg:row-[1] lg:h-auto">
         <img
           src={`/images/museums/${id}.webp`}
           alt={`${name} image`}
-          className="absolute object-cover w-full h-full rounded-t-3xl"
-          style={{
-            maskImage: 'linear-gradient(black 60%, transparent)',
-          }}
+          className="absolute object-cover w-full h-full rounded-t-3xl lg:rounded-r-3xl lg:rounded-t-none lg:block"
         />
-        <div className="relative flex h-full items-end justify-between pb-2">
+        <div className="relative flex h-full items-end justify-between pb-2 lg:hidden">
           <span className="bg-black/40 text-gray-200 p-1.5 rounded-md m-1">
             {price.regular === 0
               ? 'Gratis'
@@ -47,6 +49,18 @@ function CardMuseum({
       </div>
       <div className="grid gap-1.5 p-4 lg:col-span-3">
         <h2 className="text-lg font-bold">{name}</h2>
+        <div className="hidden lg:flex gap-3 font-medium">
+          <span className="">
+            {price.regular === 0
+              ? 'Gratis'
+              : `$${price.regular} MXN`
+            }
+          </span>
+          <span>⁝</span>
+          <span className="">
+            {zone}
+          </span>
+        </div>
         <p className="text-text-200 line-clamp-1">{address}</p>
         <div>
           {
