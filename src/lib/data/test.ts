499ca44319b072365aaa1b47526d8d5846d9ad52
CarlosGunter <carlosgt1702@gmail.com>
2025-03-24 22:48:20 -0600

    fix: los acentos en los dias provocaba resultados erroneos

--- src/lib/data/test.ts
+++ src/lib/data/test.ts
@@ -7,7 +7,7 @@ export const data: TestMuseo[] = [
     address: 'Av. Zempoaltecas s/n esq. Av. Manuel Salazar, Col. Ex Hacienda El Rosario, CP 02420, Azcapotzalco, Ciudad de México',
     zone: 'Norte',
     theme: 'Historia',
-    days: ['Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+    days: ['Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
     schedule: [
       'Martes a viernes: 9:00 - 17:00',
       'Sábado y domingo: 10:00 - 15:30'
@@ -26,7 +26,7 @@ export const data: TestMuseo[] = [
     address: 'Hacienda del Rosario s/n, Col. Prados del Rosario, CP 02420, Azcapotzalco, Ciudad de México',
     zone: 'Norte',
     theme: 'Historia',
-    days: ['Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+    days: ['Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
     schedule: [
       'Martes a domingo: 10:00 - 17:00'
     ],
@@ -40,7 +40,7 @@ export const data: TestMuseo[] = [
     address: 'Av. Universidad 1330, Col. Del Carmen, CP 04100, Coyoacán, Ciudad de México',
     zone: 'Sur',
     theme: 'Arte',
-    days: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'],
+    days: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'],
     schedule: [
       'Lunes a viernes: 16:00 - 19:00'
     ],
@@ -54,7 +54,7 @@ export const data: TestMuseo[] = [
     address: 'Av. Río Churubusco 410, Col. Del Carmen Coyoacán, CP 04100, Coyoacán, Ciudad de México',
     zone: 'Sur',
     theme: 'Historia',
-    days: ['Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+    days: ['Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
     schedule: [
       'Martes a domingo: 10:00 - 17:00'
     ],
@@ -72,7 +72,7 @@ export const data: TestMuseo[] = [
     address: 'Museo 150, Col. San Pablo Tepetlapa, CP 04620, Coyoacán, Ciudad de México',
     zone: 'Sur',
     theme: 'Cultura',
-    days: ['Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+    days: ['Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
     schedule: [
       'Martes a domingo: 11:00 - 18:00'
     ],
