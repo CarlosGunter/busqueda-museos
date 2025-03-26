c7d643f00a140e72b319cb68dcaba6886c710e87
CarlosGunter <carlosgt1702@gmail.com>
2025-03-26 01:53:24 -0600

    feat: el esquema de validación de la API ahora evalua un parámetro para paginación

--- src/lib/schemas/searchSchema.ts
+++ src/lib/schemas/searchSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
-import { DAYS_VALUES, THEME_VALUES, ZONE_VALUES } from '@/utils/consts'
+import { DAYS_VALUES, THEME_VALUES, ZONE_VALUES, MAX_PAGES } from '@/utils/consts'
 
-/** Declaración del esquema de consulta Backend */
+/** Declaración del esquema de consulta a la API */
 export const querySchema = z.object({
   zona: z.enum(ZONE_VALUES).optional(),
   tema: z.enum(THEME_VALUES).optional(),
@@ -10,7 +10,12 @@ export const querySchema = z.object({
     .transform((value) => value.split(';').map((day) => day.trim()))
     .pipe(z.enum(DAYS_VALUES).array())
     .optional(),
-  precio: z.literal('gratis').optional()
+  precio: z.literal('gratis').optional(),
+  page: z
+    .string()
+    .transform((value) => parseInt(value))
+    .pipe(z.number().int().min(1).max(MAX_PAGES))
+    .optional()
 }).strict()
 
 /** Declaración del esquema de consulta Frontend */
